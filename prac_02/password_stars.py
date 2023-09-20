932abb8f0bcac160b4ed6202fa14f97d03f4b120
Lucas <lucas.obrien@my.jcu.edu.au>
2023-09-20 11:11:12 +1000

    Refactored password check program to have functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,12 +1,40 @@
-"""write a program that asks the user for a password, with error-checking to repeat if the password doesn't
- meet a minimum length set by a variable."""
+"""
+function main
+    password = get valid password
+    print asterisks
+
+function print stars
+    print asterisks equal to length of word
+
+function get valid password
+    get password
+    if password is less than minimum length
+        display invalid password
+        get password
+    return password
+"""
 
 MINIMUM_PASSWORD_LENGTH = 5
 
-password = input("Enter a password: ")
 
-if len(password) < MINIMUM_PASSWORD_LENGTH:
-    print("Password too short")
+def main():
+    """Has the user input a password, then prints an amount of * equal to the length of the password."""
+    password = get_password()
+    print_stars(password)
+
+
+def print_stars(word):
+    """Print * equal to the length of string passed in."""
+    print("*" * len(word))
+
+
+def get_password():
+    """Gets a password, then checks if the password meets a minimum length, then returns password if valid"""
     password = input("Enter a password: ")
+    if len(password) < MINIMUM_PASSWORD_LENGTH:
+        print("Password too short")
+        password = input("Enter a password: ")
+    return password
 
-print("*" * len(password))
+
+main()
