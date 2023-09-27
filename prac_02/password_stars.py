ed81868327fa37a9f23c1a9383f3c1d7dc93a1fb
Lucas <lucas.obrien@my.jcu.edu.au>
2023-09-27 11:31:37 +1000

    Add comments for future revision

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -16,7 +16,7 @@ function get valid password
 
 MINIMUM_PASSWORD_LENGTH = 5
 
-
+# Think about refactoring this for use elsewhere
 def main():
     """Program that has the user input a password, validate it, then prints an amount of * equal to the length of
         the password."""
@@ -32,7 +32,7 @@ def print_stars(word):
 def get_password():
     """Gets a password, then checks if the password meets a minimum length, then returns password if valid"""
     password = input("Enter a password: ")
-    if len(password) < MINIMUM_PASSWORD_LENGTH:
+    if len(password) < MINIMUM_PASSWORD_LENGTH:  # think about changing this to variables for reusability
         print("Password too short")
         password = input("Enter a password: ")
     return password
