a9fa7f4a2345c5860093401d1a4e728002146fd2
Lucas <lucas.obrien@my.jcu.edu.au>
2023-10-04 09:32:46 +1000

    Add part 2 of list exercises, comment out first part for testing

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -2,14 +2,17 @@
 The program should then output information about these numbers, as in the output below.
 You can use the functions min, max, sum and len, and you can use the append method to add a number to a list."""
 
-numbers = []
+# numbers = []
+#
+# for i in range(5):
+#     number = int(input("Number: "))
+#     numbers.append(number)
+#
+# print(f"The first number is {numbers[0]}")
+# print(f"The last number is {numbers[-1]}")
+# print(f"The smallest number is {min(numbers)}")
+# print(f"The largest number is {max(numbers)}")
+# print(f"The average number is {sum(numbers)/len(numbers)}")
 
-for i in range(5):
-    number = int(input("Number: "))
-    numbers.append(number)
-
-print(f"The first number is {numbers[0]}")
-print(f"The last number is {numbers[-1]}")
-print(f"The smallest number is {min(numbers)}")
-print(f"The largest number is {max(numbers)}")
-print(f"The average number is {sum(numbers)/len(numbers)}")
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
