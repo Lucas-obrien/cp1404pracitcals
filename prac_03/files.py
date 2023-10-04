12a73043488591bc58c8fe14897ea15e1706436d
Lucas <lucas.obrien@my.jcu.edu.au>
2023-10-04 11:05:49 +1000

    Add comments for code improvement

--- prac_03/files.py
+++ prac_03/files.py
@@ -11,12 +11,13 @@ with open("name.txt", "r") as input_file:
 with open("numbers.txt", "r") as input_file:
     first_number = int(input_file.readline())
     second_number = int(input_file.readline())
-    print(first_number + second_number)
+    print(first_number + second_number)  # move outside the loop
 
 # Question 4:
 with open("numbers.txt", "r") as input_file:
-    numbers = input_file.readlines()
+    # should be named lines, numbers implies its numbers when they're actually lines/strings
+    numbers = input_file.readlines()  # should be for line in file, did'nt need, shouldve done in lower loop
     sum_of_numbers = 0
     for number in numbers:
-        sum_of_numbers += float(number) # assumes future numbers could be non-int
+        sum_of_numbers += float(number)  # assumes future numbers could be non-int
     print(f"Total sum of list is {sum_of_numbers}")
