b2517e29e0c68fd9f8ffe44bf66c9e3b53e6abb4
Lucas <lucas.obrien@my.jcu.edu.au>
2023-10-04 11:27:08 +1000

    Change numbers to large_numbers

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -35,10 +35,12 @@ lowercase_full_names = [name.lower() for name in full_names]
 almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
 # list comprehension to create a list of integers from the above list of strings
 numbers = [int(number) for number in almost_numbers]
+print(numbers)
 
 # list comprehension to create a list of only the numbers that are
 # greater than 9 from the numbers (not strings) you just created
-numbers = [number for number in numbers if number > 9]
+large_numbers = [number for number in numbers if number > 9]
+print(large_numbers)
 
 # (more advanced) use a list comprehension and the join string method
 # to create a string (not list) of the last names for those full names longer than 11 characters
