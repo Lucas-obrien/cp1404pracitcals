231c0121fec353f1fee568636a409600cd1c49cf
Lucas <lucas.obrien@my.jcu.edu.au>
2023-10-11 09:15:36 +1000

    Encapsulate previous code in while loop

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -8,15 +8,16 @@ Actual:
 name_to_email = {}
 
 email_input = "lucas.obrien@gmail.com" #input("Email: ")
-email_detail = email_input.split("@")
-# name = email_detail[0].split(".")
+while email_input != "":
+    email_detail = email_input.split("@")
+    # name = email_detail[0].split(".")
 
-name = ' '.join(email_detail[0].split('.')).title()
+    name = ' '.join(email_detail[0].split('.')).title()
 
-if input(f'Is your name {name}') != "":
-    name = input("Name: ")
+    if input(f'Is your name {name}') != "":
+        name = input("Name: ")
 
-name_to_email[name] = email_input
+    name_to_email[name] = email_input
+    email_input = input("Email: ")
 print(name_to_email)
-
 
