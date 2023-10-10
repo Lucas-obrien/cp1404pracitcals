833ac0f39bb53d9f54a1516b66d681f5af337e7b
Lucas <lucas.obrien@my.jcu.edu.au>
2023-10-11 09:44:30 +1000

    Imported csv and add for loop to add winners to dictionary

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -4,7 +4,20 @@ estimate: 30 mins
 actual:
 
 """
+import csv
 
+champion_to_wins = {}
+wimbledon_finals = []
 with open("wimbledon.csv", "r", encoding="utf-8-sig") as in_file:
-    for row in in_file:
-        print(row)
+    in_file.readline()
+    rows = csv.reader(in_file, delimiter=",")
+    for row in rows:
+        wimbledon_finals.append(row)
+
+for match in wimbledon_finals:
+    try:
+        champion_to_wins[match[2]] += 1
+    except KeyError:
+        champion_to_wins[match[2]] = 1
+print(wimbledon_finals)
+print(champion_to_wins)
