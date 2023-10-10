8c36fef002eac91d86ca5fb9981efcda6ba55178
Lucas <lucas.obrien@my.jcu.edu.au>
2023-10-11 08:32:35 +1000

    Add loop to count words in text

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -5,3 +5,13 @@ Actual:
 
 """
 
+text = "this is a collection of words of nice words this is a fun thing it is"
+
+word_to_count = {}
+
+text = text.split()
+for word in text:
+    try:
+        word_to_count[word] += 1
+    except KeyError:
+        word_to_count[word] = 1
