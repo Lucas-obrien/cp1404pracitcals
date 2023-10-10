7448bbd289e82aa7fc798af3f5b799bc84f64cc5
Lucas <lucas.obrien@my.jcu.edu.au>
2023-10-11 09:20:21 +1000

    Add print for the dictionary keys and values

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -2,22 +2,22 @@
 Emails
 Estimate: 30 mins
 Actual:
-
 """
 
 name_to_email = {}
 
-email_input = "lucas.obrien@gmail.com" #input("Email: ")
+email_input = input("Email: ")
 while email_input != "":
     email_detail = email_input.split("@")
     # name = email_detail[0].split(".")
 
     name = ' '.join(email_detail[0].split('.')).title()
 
-    if input(f'Is your name {name}') != "":
+    if input(f'Is your name {name}? (Y/n) ').upper() != "Y":
         name = input("Name: ")
 
     name_to_email[name] = email_input
     email_input = input("Email: ")
-print(name_to_email)
+for name in name_to_email:
+    print(f"{name} ({name_to_email[name]})")
 
