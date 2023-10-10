7f5a724fec0c04774046ba77185505906b2d38f2
Lucas <lucas.obrien@my.jcu.edu.au>
2023-10-11 08:16:17 +1000

    Add dictionary of colours

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,13 @@
+NAME_TO_CODE = {
+    "Absolute Zero": "#0048ba",
+    "Bitter Lemon": "#cae00d",
+    "Celadon": "#ace1af",
+    "Deep Peach": "#ffcba4",
+    "Eggshell": "#f0ead6",
+    "Ferrari Red": "#ff2800",
+    "Glaucous": "#6082b6",
+    "Harlequin": "#3fff00",
+    "Iceberg": "#71a6d2",
+    "Jungle Green": "4cbb17"
+}
+
