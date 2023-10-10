f92e8b91593ba930224bd7f9d515ba640bf7116a
Lucas <lucas.obrien@my.jcu.edu.au>
2023-10-11 09:13:39 +1000

    Add listed variables to split email address at @ symbol and fullstops

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -4,3 +4,14 @@ Estimate: 30 mins
 Actual:
 
 """
+
+name_to_email = {}
+
+email_input = "lucas.obrien@gmail.com" #input("Email: ")
+email_detail = email_input.split("@")
+# name = email_detail[0].split(".")
+
+name = ' '.join(email_detail[0].split('.')).title()
+
+
+
