9e2ab0e0301c5e538288f7e869223c5b2318e8a5
Lucas <lucas.obrien@my.jcu.edu.au>
2023-10-11 11:20:04 +1000

    Add empty dictionary, test email to check code, code to add to dictionary and display

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -4,4 +4,18 @@ Estimate: 30 mins
 Actual: 23:47
 """
 
+name_to_email = {}
+
+
+email = "lucas.obrien@gmail.com" #input("Email: ")
+email = email.split("@")
+name = ' '.join(email[0].split('.')).title()
+choice = input(f'Is your name {name}? (Y/n) ').upper()
+if choice != "Y" and choice != "":
+    name = input("Name: ")
+name_to_email[name] = "@".join(email)
+email = input("Email: ")
+
+for name, email in name_to_email.items():
+    print(f"{name} ({email})")
 
