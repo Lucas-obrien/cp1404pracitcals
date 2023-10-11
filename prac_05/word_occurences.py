4178bf363b9e954f56a3ea92d82d7f65fdf622bd
Lucas <lucas.obrien@my.jcu.edu.au>
2023-10-11 11:26:28 +1000

    Add text variable, test code to check split is working

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -4,4 +4,8 @@ Estimate: 20 minutes
 Actual:
 
 """
-
+text = "this is a collection of words of nice words this is a fun thing it is"
+word_to_count = {}
+print(text)
+text = text.split()
+print(text)
