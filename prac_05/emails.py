3f4184d4b85c66bac6980c173bc4a5ccd4b687f2
Lucas <lucas.obrien@my.jcu.edu.au>
2023-10-11 11:00:12 +1000

    Change dictionary adding section to be a join rather than a separate variable, refactor two variables into one

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -6,16 +6,15 @@ Actual: 23:47
 
 name_to_email = {}
 
-email_input = input("Email: ")
-while email_input != "":
-    email_details = email_input.split("@")
-    name = ' '.join(email_details[0].split('.')).title()
-
+email = input("Email: ")
+while email != "":
+    email = email.split("@")
+    name = ' '.join(email[0].split('.')).title()
     if input(f'Is your name {name}? (Y/n) ').upper() != "Y":
         name = input("Name: ")
+    name_to_email[name] = "@".join(email)
+    email = input("Email: ")
 
-    name_to_email[name] = email_input
-    email_input = input("Email: ")
 for name, email in name_to_email.items():
     print(f"{name} ({email})")
 
