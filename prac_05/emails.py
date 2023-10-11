2d510b6818be33238be93c6f38d666e261f049d2
Lucas <lucas.obrien@my.jcu.edu.au>
2023-10-11 11:20:46 +1000

    Add loop to satisfy prac 5 requirements, complete prac 5

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -7,15 +7,15 @@ Actual: 23:47
 name_to_email = {}
 
 
-email = "lucas.obrien@gmail.com" #input("Email: ")
-email = email.split("@")
-name = ' '.join(email[0].split('.')).title()
-choice = input(f'Is your name {name}? (Y/n) ').upper()
-if choice != "Y" and choice != "":
-    name = input("Name: ")
-name_to_email[name] = "@".join(email)
 email = input("Email: ")
+while email != "":
+    email = email.split("@")
+    name = ' '.join(email[0].split('.')).title()
+    choice = input(f'Is your name {name}? (Y/n) ').upper()
+    if choice != "Y" and choice != "":
+        name = input("Name: ")
+    name_to_email[name] = "@".join(email)
+    email = input("Email: ")
 
 for name, email in name_to_email.items():
     print(f"{name} ({email})")
-
