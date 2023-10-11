97e0916c81152b2b8fb74c88589a34dc22d7e471
Lucas <lucas.obrien@my.jcu.edu.au>
2023-10-11 11:44:41 +1000

    Add variables and open file code

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -4,3 +4,15 @@ estimate: 30 mins
 actual:
 
 """
+import csv
+
+champion_to_wins = {}
+wimbledon_finals = []
+countries = []
+
+
+with open("wimbledon.csv", "r", encoding="utf-8-sig") as in_file:
+    in_file.readline()
+    rows = csv.reader(in_file, delimiter=",")
+    for row in rows:
+        wimbledon_finals.append(row)
