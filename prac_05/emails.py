b73e5e4c42e35ae5b48b67f067ac4c65a6a241c0
Lucas <lucas.obrien@my.jcu.edu.au>
2023-10-11 11:18:29 +1000

    Add emails for prac 5

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -4,18 +4,4 @@ Estimate: 30 mins
 Actual: 23:47
 """
 
-name_to_email = {}
-
-email = input("Email: ")
-while email != "":
-    email = email.split("@")
-    name = ' '.join(email[0].split('.')).title()
-    choice = input(f'Is your name {name}? (Y/n) ').upper()
-    if choice != "Y" and choice != "":
-        name = input("Name: ")
-    name_to_email[name] = "@".join(email)
-    email = input("Email: ")
-
-for name, email in name_to_email.items():
-    print(f"{name} ({email})")
 
