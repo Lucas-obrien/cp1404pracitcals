a74fe939ffcd77551787e7198a13a8a28bc266a0
Lucas <lucas.obrien@my.jcu.edu.au>
2023-10-11 11:24:28 +1000

    Add word occurrences for prac 5

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -1,22 +1,7 @@
 """
 Word Occurrences
 Estimate: 20 minutes
-Actual: 21:08
+Actual:
 
 """
 
-text = "this is a collection of words of nice words this is a fun thing it is"
-
-word_to_count = {}
-print(text)
-text = text.split()
-for word in text:
-    try:
-        word_to_count[word] += 1
-    except KeyError:
-        word_to_count[word] = 1
-longest_word = max((len(word)) for word in word_to_count)
-
-for word in sorted(word_to_count):
-    print(f"{word:{longest_word}} : {word_to_count[word]}")
-print(type(word_to_count))
