e88a9e7473e762b89f8463492f692a8a69d13cc6
Lucas <lucas.obrien@my.jcu.edu.au>
2023-10-11 10:47:49 +1000

    Add docstrings to functions, complete wimbledon prac 5

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -8,42 +8,43 @@ import csv
 
 
 def main():
+    """Take in a list of Wimbledon final matches, then process and display the winners"""
     champion_to_wins = {}
-    winning_countries = []
     wimbledon_finals = []
     get_winners(wimbledon_finals)
-
-    winning_countries = compile_winners(champion_to_wins, wimbledon_finals, winning_countries)
-
+    winning_countries = compile_winners(champion_to_wins, wimbledon_finals)
     display_winners(champion_to_wins, winning_countries)
 
 
-def display_winners(champion_to_wins, winning_countries):
-    print("Wimbledon Champions:")
-    for champion, win in champion_to_wins.items():
-        print(f"{champion} : {win}")
-    print(f"These {len(winning_countries)} countries have won Wimbledon:")
-    print(", ".join(country for country in sorted(winning_countries)))
-
-
-def compile_winners(champion_to_wins, wimbledon_finals, winning_countries):
-    for match in wimbledon_finals:
-        print(match)
-        try:
-            winning_countries.append(match[1])
-            champion_to_wins[match[2]] += 1
-        except KeyError:
-            champion_to_wins[match[2]] = 1
-    winning_countries = set(winning_countries)
-    return winning_countries
-
-
 def get_winners(wimbledon_finals):
+    """Open a csv file, read and then append to a list"""
     with open("wimbledon.csv", "r", encoding="utf-8-sig") as in_file:
         in_file.readline()
         rows = csv.reader(in_file, delimiter=",")
         for row in rows:
             wimbledon_finals.append(row)
+
+
+def compile_winners(champion_to_wins, wimbledon_finals):
+    """Take empty lists and dictionary then add winning countries to a list and add to count of a key or set a key's
+    value"""
+    countries = []
+    for match in wimbledon_finals:
+        try:
+            countries.append(match[1])
+            champion_to_wins[match[2]] += 1
+        except KeyError:
+            champion_to_wins[match[2]] = 1
+    return set(countries)
+
+
+def display_winners(champion_to_wins, winning_countries):
+    """Take a dictionary of winners and a list of winning countries and display formatted"""
+    print("Wimbledon Champions:")
+    for champion, win in champion_to_wins.items():
+        print(f"{champion} : {win}")
+    print(f"These {len(winning_countries)} countries have won Wimbledon:")
+    print(", ".join(country for country in sorted(winning_countries)))
 
 
 main()
