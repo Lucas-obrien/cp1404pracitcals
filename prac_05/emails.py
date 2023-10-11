f5758ce08af66d4df6a19ac8b8aaedd580da8dc1
Lucas <lucas.obrien@my.jcu.edu.au>
2023-10-11 11:09:58 +1000

    Fix if statement to reflect Y/n correctly

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -10,7 +10,8 @@ email = input("Email: ")
 while email != "":
     email = email.split("@")
     name = ' '.join(email[0].split('.')).title()
-    if input(f'Is your name {name}? (Y/n) ').upper() != "Y":
+    choice = input(f'Is your name {name}? (Y/n) ').upper()
+    if choice != "Y" and choice != "":
         name = input("Name: ")
     name_to_email[name] = "@".join(email)
     email = input("Email: ")
