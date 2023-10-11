9deb323fd40a6ff05fb6e6537cb88b52174007f7
Lucas <lucas.obrien@my.jcu.edu.au>
2023-10-11 11:27:13 +1000

    Add loop to add/or count words in text to a dictionary

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -8,4 +8,8 @@ text = "this is a collection of words of nice words this is a fun thing it is"
 word_to_count = {}
 print(text)
 text = text.split()
-print(text)
+for word in text:
+    try:
+        word_to_count[word] += 1
+    except KeyError:
+        word_to_count[word] = 1
