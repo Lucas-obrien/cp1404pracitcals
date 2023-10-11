347cbd59de85d795cfeee467927a713a48c080e5
Lucas <lucas.obrien@my.jcu.edu.au>
2023-10-11 11:45:19 +1000

    Add loop with exception handling to append countries to list and champions to dictionary with win count

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -16,3 +16,13 @@ with open("wimbledon.csv", "r", encoding="utf-8-sig") as in_file:
     rows = csv.reader(in_file, delimiter=",")
     for row in rows:
         wimbledon_finals.append(row)
+
+
+for match in wimbledon_finals:
+    try:
+        countries.append(match[1])
+        champion_to_wins[match[2]] += 1
+    except KeyError:
+        champion_to_wins[match[2]] = 1
+
+
