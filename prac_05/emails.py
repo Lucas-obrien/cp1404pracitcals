8a4a815bdde1e351d9cf4b38d18a9d395bf485e1
Lucas <lucas.obrien@my.jcu.edu.au>
2023-10-15 07:44:22 +1000

    Change variables to lessen the work load on program

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -9,12 +9,12 @@ name_to_email = {}
 
 email = input("Email: ")
 while email != "":
-    email = email.split("@")
-    name = ' '.join(email[0].split('.')).title()
+    parts = email.split("@")
+    name = ' '.join(parts[0].split('.')).title()
     choice = input(f'Is your name {name}? (Y/n) ').upper()
     if choice != "Y" and choice != "":
         name = input("Name: ")
-    name_to_email[name] = "@".join(email)
+    name_to_email[name] = email
     email = input("Email: ")
 
 for name, email in name_to_email.items():
