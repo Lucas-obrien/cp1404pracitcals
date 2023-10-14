bd6d981376e8f9ea784825fd0f8386ff43005b64
Lucas <lucas.obrien@my.jcu.edu.au>
2023-10-15 08:58:44 +1000

    Change name to do all work on one line

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -9,12 +9,11 @@ name_to_email = {}
 
 email = input("Email: ")
 while email != "":
-    email = email.split("@")
-    name = ' '.join(email[0].split('.')).title()
+    name = " ".join(((email.split("@"))[0]).split('.'))
     choice = input(f'Is your name {name}? (Y/n) ').upper()
     if choice != "Y" and choice != "":
         name = input("Name: ")
-    name_to_email[name] = "@".join(email)
+    name_to_email[name] = email
     email = input("Email: ")
 
 for name, email in name_to_email.items():
