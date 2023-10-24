1213772cdc0d116d359d0b3e753741b2d6791213
Lucas <lucas.obrien@my.jcu.edu.au>
2023-10-25 08:34:30 +1000

    Add is_dynamic to class

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,7 +1,21 @@
-"""Module docstring"""
+"""Module docstring
+
+estimation - 30 mins
+actual -
+
+"""
 
 
 class ProgrammingLanguage:
     """Constructs programming language data fields"""
-    def __init__(self):
-        pass
+
+    def __init__(self, name="", typing="", reflection=False, year=0):
+        """Initialise programming language instance"""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        return self.reflection
+
