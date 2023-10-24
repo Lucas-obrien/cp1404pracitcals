ead2d89eff1a58a5998935c0df624645ba63fb24
Lucas <lucas.obrien@my.jcu.edu.au>
2023-10-25 09:25:17 +1000

    Add formatting for __str__ so cost prints with commas

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -4,7 +4,7 @@
 class Guitar:
     """Construct a guitar instance"""
 
-    def __init__(self, name="", year=0, cost=0):
+    def __init__(self, name="", year=0, cost=0.0):
         """Initialise guitar instance"""
         self.name = name
         self.year = year
@@ -12,12 +12,10 @@ class Guitar:
 
     def __str__(self):
         """Returns formatted string, example: Gibson L-5 CES (1922) : $16,035.40"""
-        return f"{self.name} ({self.year}) : ${self.cost}"
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
 
     def get_age(self):
         return 2023 - self.year
 
     def is_vintage(self):
         return self.get_age() > 50
-
-
