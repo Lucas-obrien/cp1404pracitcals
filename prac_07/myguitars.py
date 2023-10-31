9d4a6293a78173ccab431b0cc9b30e2a2387e1d5
Lucas <lucas.obrien@my.jcu.edu.au>
2023-11-01 09:03:38 +1000

    Add NewGuitar Subclass to tuple data type class, Change guitars.append to append the tuple list instead of object, add sort by itemgetter for year, add constant for Index of year

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -6,9 +6,12 @@ actual -
 """
 
 import csv
+from collections import namedtuple
 from guitar import Guitar
+from operator import itemgetter
 
 FILENAME = "guitars.csv"
+INDEX_YEAR = 1
 
 
 def main():
@@ -16,9 +19,10 @@ def main():
     with open(FILENAME, "r") as in_file:
         in_file.readline()
         reader = csv.reader(in_file)
+        NewGuitar = namedtuple('Guitar', 'name, year, cost')
         for row in reader:
-            guitars.append(Guitar(row[0], row[1], float(row[2])))
-
+            guitars.append(NewGuitar._make(row))
+        guitars.sort(key=itemgetter(INDEX_YEAR))
         for guitar in guitars:
             print(guitar)
 
