d761e930ed3c8a398bef641a4549697481cbc903
Lucas <lucas.obrien@my.jcu.edu.au>
2023-11-04 15:46:52 +1000

    Separate display_projects into its own function, create new display_all_current_projects for incomplete and complete projects, refactor all printing of projects to call display_projects, add sorting to filtered projects

--- prac_07/project.py
+++ prac_07/project.py
@@ -27,20 +27,25 @@ from project_management import ProjectManagement
 MENU = ("- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n- (A)dd new project\n"
         "- (U)pdate project\n- (Q)uit\n>>> ")
 DEFAULT_LOAD_FILE = 'projects.txt'
+NO_MAXIMUM = "No Max"
 DEFAULT_SAVE_FILE = 'projects.txt'
+MINIMUM_PRIORITY = 0
+MAXIMUM_PRIORITY = 9
+MINIMUM_PERCENTAGE = 0
+MAXIMUM_PERCENTAGE = 100
+MINIMUM_COST = 0
 
 
 def main():
     choice = input(MENU).upper()
-    projects, file_header_names = load_file(DEFAULT_LOAD_FILE)  # make this auto load the file
-    print(file_header_names)
+    projects, file_header_names = load_file(DEFAULT_LOAD_FILE)  # make this autoload the file
     while choice != "Q":
         if choice == "L":
-            file_header_names, projects = manual_load_file(file_header_names, projects)
+            file_header_names, projects = manual_load_file()
         elif choice == "S":
             manual_save_file(file_header_names, projects)
         elif choice == "D":
-            display_projects(projects)
+            display_all_current_projects(projects)
         elif choice == "F":
             filter_projects(projects)
         elif choice == "A":
@@ -51,21 +56,16 @@ def main():
         else:
             print("Invalid choice")
         choice = input(MENU).upper()
-    save_projects(file_header_names, projects, DEFAULT_SAVE_FILE)
-
-
-def manual_load_file(file_header_names, projects):
-    file_header_names, projects = load_user_file(file_header_names, projects)
-    return file_header_names, projects
+    save_file(file_header_names, projects, DEFAULT_SAVE_FILE)
 
 
 def manual_save_file(file_header_names, projects):
     save_file_name = input("Input file name to save to: ")
     # ignore error; won't reach here without a project being loaded
-    save_projects(file_header_names, projects, save_file_name)
+    save_file(file_header_names, projects, save_file_name)
 
 
-def load_user_file(file_header_names, projects):
+def manual_load_file():
     try:
         file_name = input("Enter file name to load: ")
         projects, file_header_names = load_file(file_name)
@@ -79,20 +79,18 @@ def update_project(projects):
     for i, project in enumerate(projects):
         print(i, project)
     choice = is_valid_project("Pick a project: ", projects)
-    print(projects[choice])
-    new_percentage = input("New percentage: ")
+    new_percentage = is_valid_number("New percentage: ", MINIMUM_PERCENTAGE, MAXIMUM_PERCENTAGE)
     if not new_percentage:
-        new_percentage \
-            = projects[choice].completion_percentage
-    new_priority = int(input("New Priority: "))
+        new_percentage = projects[choice].completion_percentage
+    new_priority = int(is_valid_number("New Priority: ", MINIMUM_PRIORITY, MAXIMUM_PRIORITY))
     if not new_priority:
         new_priority = projects[choice].priority
     projects[choice] = ProjectManagement(projects[choice].name, projects[choice].start_date, new_priority,
                                          projects[choice].cost_estimate, new_percentage)
-    print(type(projects[choice].priority))
 
 
 def is_valid_project(output_string, projects):
+    """Determine if a project exists."""
     valid_choice = False
     while not valid_choice:
         try:
@@ -101,35 +99,41 @@ def is_valid_project(output_string, projects):
                 print("Must be >= 0")
             else:
                 valid_choice = True
-
         except (IndexError, ValueError):
             print("Invalid number")
-
-    return choice
+    return choice  # Ignore error; appears due to try/except
 
 
 def add_project(projects):
+    """Add a project object to a list."""
     print("Let's add a new project")
     name = is_valid_string("name: ")
     start_date = is_valid_date("Start date (d/m/yyyy): ")  # e.g., "30/9/2022"
-    priority = is_valid_number("Priority: ")
-    cost_estimate = float(is_valid_number("Cost estimate: $"))
-    completion_percentage = is_valid_number("Completion percentage: ")
+    priority = is_valid_number("Priority: ", MINIMUM_PRIORITY, MAXIMUM_PRIORITY)
+    cost_estimate = float(is_valid_number("Cost estimate: $", MINIMUM_COST, NO_MAXIMUM))
+    completion_percentage = is_valid_number("Completion percentage: ", MINIMUM_PERCENTAGE, MAXIMUM_PERCENTAGE)
     projects.append(ProjectManagement(name, start_date, priority, cost_estimate, completion_percentage))
 
 
-def is_valid_number(output_string):
+def is_valid_number(output_string, minimum_number, maximum_number):
+    """Determine if a number is valid."""
     valid_number = False
     while not valid_number:
         try:
             number_choice = int(input(output_string))
-            valid_number = True
+            if minimum_number <= number_choice <= maximum_number:
+                valid_number = True
+            else:
+                error_string = f"between {minimum_number} and {maximum_number} inclusive" if (
+                        maximum_number != NO_MAXIMUM) else f"greater than {minimum_number}"
+                print(f"Number must be {error_string}")
         except ValueError:
             print("Invalid number")
     return number_choice
 
 
 def is_valid_string(input_string):
+    """Determine if a string is valid."""
     valid_string = False
     while not valid_string:
         try:
@@ -140,11 +144,11 @@ def is_valid_string(input_string):
                 valid_string = True
         except ValueError:
             print(f"Invalid {input_string}")
-
     return choice
 
 
 def is_valid_date(output_string):
+    """Determine if a date is valid."""
     valid_date = False
     while not valid_date:
         try:
@@ -157,18 +161,19 @@ def is_valid_date(output_string):
 
 
 def filter_projects(projects):
+    """Sort projects based on an entered date value"""
     try:
         date_string = input("Show projects that start after date (d/m/yyyy): ")
         filter_date = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()  # e.g., "30/9/2022"
         filtered_projects = [project for project in projects
                              if datetime.datetime.strptime(project.start_date, "%d/%m/%Y").date() > filter_date]
-        for project in filtered_projects:
-            print(project)
+        filtered_projects.sort(key=lambda project: project.start_date, reverse=True)
+        display_projects(filtered_projects)
     except ValueError:
         print("Incorrect format for date")
 
 
-def save_projects(file_header_names, projects, save_file_name):
+def save_file(file_header_names, projects, save_file_name):
     with open(save_file_name, "w") as out_file:
         print(file_header_names, file=out_file)
         for project in projects:
@@ -183,21 +188,25 @@ def load_file(file_name):
         file_header_names = in_file.readline().strip()
         reader = csv.reader(in_file, delimiter='\t')
         for row in reader:
-            print(row)
             projects.append(ProjectManagement(row[0], row[1], int(row[2]), float(row[3]), int(row[4])))
     return projects, file_header_names
 
 
-def display_projects(projects):
+def display_all_current_projects(projects):
+    """Create two lists of complete/incomplete projects and display."""
     incomplete_projects = [project for project in projects if project.is_incomplete()]
-    incomplete_projects = sorted(incomplete_projects, key=lambda project: project.priority)
+    incomplete_projects.sort(key=lambda project: project.priority)
+    print("Incomplete projects: ")
+    display_projects(incomplete_projects)
     complete_projects = [project for project in projects if project not in incomplete_projects]
-    complete_projects = sorted(complete_projects, key=lambda project: project.priority)
-    print("Incomplete projects:")
-    for project in incomplete_projects:
-        print(project)
-    print("Completed projects:")
-    for project in complete_projects:
+    complete_projects.sort(key=lambda project: project.priority)
+    print("Complete projects: ")
+    display_projects(complete_projects)
+
+
+def display_projects(projects):
+    """Print passed in projects."""
+    for project in projects:
         print(project)
 
 
