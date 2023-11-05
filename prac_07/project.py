0057e7004d917317052f5ee97e83e7f36b5e1e8d
Lucas <lucas.obrien@my.jcu.edu.au>
2023-11-05 11:32:40 +1000

    Refactor function names to represent what they return, alter docstrings to reflect new names, change get_valid_number to correctly set valid_number to True when a cost with no maximum is entered.

--- prac_07/project.py
+++ prac_07/project.py
@@ -15,7 +15,8 @@ from project_management import ProjectManagement
 MENU = ("- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date"
         "\n- (A)dd new project\n- (U)pdate project\n- (Q)uit\n>>> ")
 DEFAULT_LOAD_FILE = 'projects.txt'
-NO_MAXIMUM = True   # Used as an alternative instead of setting a MAXIMUM_COST
+DEFAULT_FILE_HEADER_NAMES = "Name\tStart Date\tPriority\tCost Estimate\tCompletion Percentage\t"
+NO_MAXIMUM = False  # Used as an alternative instead of setting a MAXIMUM_COST
 DEFAULT_SAVE_FILE = 'projects.txt'
 MINIMUM_PRIORITY = 0
 MAXIMUM_PRIORITY = 9
@@ -49,20 +50,21 @@ def main():
 
 
 def manual_save_file(file_header_names, projects):
-    """Prompt user for a file name, then write list into it."""
+    """Prompt for a file name, then write list into it."""
     save_file_name = input("Input file name to save to: ")
     # ignore error; won't reach here without a project being loaded
     save_file(file_header_names, projects, save_file_name)
 
 
 def manual_load_file():
-    """Prompt user for a file name, then try to load, on fail set list to blank."""
+    """Prompt for a file name, then try to load, on fail set list to blank."""
     try:
         file_name = input("Enter file name to load: ")
         projects, file_header_names = load_file(file_name)
     except FileNotFoundError:
         print("File not found, loading blank list")
         projects = []
+        file_header_names = DEFAULT_FILE_HEADER_NAMES.strip()
     return file_header_names, projects  # Ignore error; appears due to try/except
 
 
@@ -71,10 +73,10 @@ def update_project(projects):
     for i, project in enumerate(projects):
         print(i, project)
     choice = is_valid_project("Pick a project: ", projects)
-    new_percentage = is_valid_number("New percentage: ", MINIMUM_PERCENTAGE, MAXIMUM_PERCENTAGE)
+    new_percentage = get_valid_number("New percentage: ", MINIMUM_PERCENTAGE, MAXIMUM_PERCENTAGE)
     if not new_percentage:
         new_percentage = projects[choice].completion_percentage
-    new_priority = int(is_valid_number("New Priority: ", MINIMUM_PRIORITY, MAXIMUM_PRIORITY))
+    new_priority = int(get_valid_number("New Priority: ", MINIMUM_PRIORITY, MAXIMUM_PRIORITY))
     if not new_priority:
         new_priority = projects[choice].priority
     projects[choice] = ProjectManagement(projects[choice].name, projects[choice].start_date,
@@ -83,7 +85,7 @@ def update_project(projects):
 
 
 def is_valid_project(output_string, projects):
-    """Determine if a project exists."""
+    """Get a valid project choice."""
     valid_choice = False
     while not valid_choice:
         try:
@@ -100,35 +102,38 @@ def is_valid_project(output_string, projects):
 def add_project(projects):
     """Add a project object to a list."""
     print("Let's add a new project")
-    name = is_valid_string("name: ")
-    start_date = is_valid_date("Start date (d/m/yyyy): ")  # e.g., "30/9/2022"
-    priority = is_valid_number("Priority: ", MINIMUM_PRIORITY, MAXIMUM_PRIORITY)
-    cost_estimate = float(is_valid_number("Cost estimate: $", MINIMUM_COST, NO_MAXIMUM))
-    completion_percentage = is_valid_number("Completion percentage: ",
-                                            MINIMUM_PERCENTAGE, MAXIMUM_PERCENTAGE)
+    name = get_valid_string("name: ")
+    start_date = get_valid_date("Start date (d/m/yyyy): ")  # e.g., "30/9/2022"
+    priority = get_valid_number("Priority: ", MINIMUM_PRIORITY, MAXIMUM_PRIORITY)
+    cost_estimate = float(get_valid_number("Cost estimate: $", MINIMUM_COST, NO_MAXIMUM))
+    completion_percentage = get_valid_number("Completion percentage: ",
+                                             MINIMUM_PERCENTAGE, MAXIMUM_PERCENTAGE)
     projects.append(ProjectManagement(name, start_date, priority,
                                       cost_estimate, completion_percentage))
 
 
-def is_valid_number(output_string, minimum_number, maximum_number):
-    """Determine if a number is valid."""
+def get_valid_number(output_string, minimum_number, maximum_number):
+    """Get a valid number between a range."""
     valid_number = False
     while not valid_number:
         try:
-            number_choice = int(input(output_string))
-            if minimum_number <= number_choice <= maximum_number:
+            # or statement for validating cost, as no cost maximum has been explicitly stated,
+            # so parameter is set to false.
+            if (minimum_number <= number_choice <= maximum_number or
+                    number_choice >= minimum_number and not maximum_number):
                 valid_number = True
             else:
                 error_string = f"between {minimum_number} and {maximum_number} inclusive" if (
                         maximum_number != NO_MAXIMUM) else f"greater than or equal to {minimum_number}"
                 print(f"Number must be {error_string}")
+            number_choice = int(input(output_string))
         except ValueError:
             print("Invalid number")
     return number_choice  # Ignore error; appears due to try/except
 
 
-def is_valid_string(input_string):
-    """Determine if a string is valid."""
+def get_valid_string(input_string):
+    """Get a valid string."""
     valid_string = False
     while not valid_string:
         string_choice = input(input_string)
@@ -139,8 +144,8 @@ def is_valid_string(input_string):
     return string_choice  # Ignore error; won't reach return due to sentinel
 
 
-def is_valid_date(output_string):
-    """Determine if a date is valid."""
+def get_valid_date(output_string):
+    """Get a valid date."""
     valid_date = False
     while not valid_date:
         try:
@@ -161,7 +166,7 @@ def filter_projects(projects):
                              if datetime.datetime.strptime(project.start_date, "%d/%m/%Y").date()
                              > filter_date]
         filtered_projects.sort(key=lambda project: project.start_date, reverse=True)
-        display_projects(filtered_projects)
+        display_project_objects(filtered_projects)
     except ValueError:
         print("Incorrect format for date")
 
@@ -193,14 +198,14 @@ def display_all_current_projects(projects):
     incomplete_projects = [project for project in projects if project.is_incomplete()]
     incomplete_projects.sort(key=lambda project: project.priority)
     print("Incomplete projects: ")
-    display_projects(incomplete_projects)
+    display_project_objects(incomplete_projects)
     complete_projects = [project for project in projects if project not in incomplete_projects]
     complete_projects.sort(key=lambda project: project.priority)
     print("Complete projects: ")
-    display_projects(complete_projects)
+    display_project_objects(complete_projects)
 
 
-def display_projects(projects):
+def display_project_objects(projects):
     """Print passed in projects."""
     for project in projects:
         print(project)
