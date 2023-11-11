3e266ce23e442c2d74ccbfa86be0bcef86b7b030
Lucas <lucas.obrien@my.jcu.edu.au>
2023-11-11 19:51:08 +1000

    Add Docstrings to functions.

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -3,15 +3,19 @@ from kivy.lang import Builder
 
 
 class BoxLayoutDemo(App):
+    """BoxLayout Demo."""
     def build(self):
+        """Build a display box"""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
+        """Return hello and text input."""
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def handle_clear(self):
+        """Clear text from ids in app."""
         self.root.ids.output_label.text = ""
         self.root.ids.input_name.text = ""
 
--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -1,22 +1,33 @@
+"""
+Dynamic Label App
+
+Estimate : 30 minutes
+Actual : 40 minutes
+"""
+
+
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.button import Label
 
 
 class DynamicLabels(App):
+    """Construct Dynamic Labels App."""
 
     def __init__(self, **kwargs):
-        """Construct main app."""
+        """Initialise list of names."""
         super().__init__(**kwargs)
         self.names = ["Lucas", "Cameron", "Hayden", "Faith", "Chandler"]  # Add siblings to names
 
     def build(self):
+        """Build Dynamic Labels."""
         self.title = "Dynamic Widgets"
         self.root = Builder.load_file('dynamic_widgets.kv')
         self.create_widgets()
         return self.root
 
     def create_widgets(self):
+        """Create a widget for each name in a list."""
         for name in self.names:
             temp_label = Label(text=name)
             print(f"Hello {name}")
