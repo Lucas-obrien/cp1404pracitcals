ce03d7682c573ae0a7bf193638fa38975a050bb5
Lucas <lucas.obrien@my.jcu.edu.au>
2023-11-15 11:51:29 +1000

    Add docstrings, change drive to call super().drive()

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -1,23 +1,29 @@
+"""
+Constructor for an Unreliable car
+
+Estimate: 15 mins
+Actual: 20 mins
+
+"""
+
 from car import Car
 import random
 
 
 class UnreliableCar(Car):
+    """Constructor for an UnreliableCar"""
 
     def __init__(self, name, fuel, reliability):
+        """Initialise a Car, then give reliability."""
         Car.__init__(self, name, fuel)
         self.reliability = reliability
 
     def drive(self, distance):
+        """Check to see if car starts, then drive"""
         if self.reliability < float(random.randint(0, 100)):
             distance = 0
         else:
-            if distance > self.fuel:
-                distance = self.fuel
-                self.fuel = 0
-            else:
-                self.fuel -= distance
-            self.odometer += distance
+            super().drive(distance)
         return distance
 
     def __str__(self):
